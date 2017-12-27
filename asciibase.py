eeb36a89463f80426e93e6849941d8afd4437447
T279 <technetium279@gmail.com>
2017-12-26 22:32:40 -0500

    Insert standard commit message here

--- asciibase.py
+++ asciibase.py
@@ -5,11 +5,13 @@ By Sam Rooney
 Goal:
 Make a game engine that can use ASCII to make a multi-level map, generate items
 for the map, and otherwise generate an ASCII-based roguelike landscape. Should
-also contain a basic player/npc framework.
+also contain a basic player/npc framework as well as an item class.
 '''
 
 #imports
-import random
+import random, pygame, sys
+
+#variables
 
 #classes
 class Player():
@@ -20,17 +22,36 @@ class Player():
         self.inventory = []
         self.clothing = []
         self.holding = []
-        self.location = startlocation 
+        self.location = startlocation
+    #pretty self-explanatory
 
     def move(self, direction):
         #direction is string, must be 'n', 's', 'e', 'w', 'nw', 'ne' etc
-        if direction == 'n':
-            direction[1] += 1
-        elif direction == 's':
-            direction[1] -= 1
-        elif direction == 'e':
-            direction[0] -= 1
-        elif direction == 'w':
-            direction[0] += 1
-        else:
-            raise ValueError('Invalid direction')
+        #im kinda proud of this sneaky buggering around of mine
+        #if the direction is invalid, nothing happens
+        if direction in ['n', 'ne', 'nw']:
+            self.location[1] += 1
+        if direction in ['s', 'se', 'sw']:
+            self.location[1] -= 1
+        if direction in ['e', 'ne', 'se']:
+            self.location[0] -= 1
+        if direction in ['w', 'nw', 'sw']:
+            self.location[0] += 1
+    #direction is string, e.g. 'n', 's', 'ne', etc
+    #if direction is invalid nothing happens
+
+
+class Item():
+    #basic item class, esp. for basic items. Contains 'id' variable, should be
+    #a unique index from 'ids' list.
+    def __init__(self, name, idsindex=None):
+        self.id = idsindex
+        self.name = name #special name if there is any
+
+
+class Game():
+    def __init__():
+        #width and height are attributes
+        screensize = self.width, self.height = 1080, 720
+        self.screen = pygame.display.set_mode(screensize)
+        
