7276c28cf500fc6602745279c0aff68e4f060665
T279 <technetium279@gmail.com>
2018-01-03 19:17:35 -0500

    continued work on config stuff

--- Improved/asciibase.py
+++ Improved/asciibase.py
@@ -6,21 +6,53 @@ Goal:
 Make an ASCII game engine. Simple as that.
 '''
 
-import random
+import random, sys, os
+
+
+def getConfig(configfile='config.txt', debug=False):
+    name = os.path.basename(__file__)
+    try:
+        config = open(__file__[:0-len(name)]+configfile, 'r')
+    except:
+        defaults = open(__file__[:0-len(name)]+"defaultconfig.txt", 'r')
+        config = open(__file__[:0-len(name)]+configfile, 'w')
+        config.write(defaults.read())
+        defaults.close()
+        config.close
+        config = open(__file__[:0-len(name)]+configfile, 'r')
+
+    configdict = {}
+
+    for ln in config:
+        ln = ln.strip()
+        if ln == '' or ln[0] == '#':
+            if debug:
+                print('skipping line: ' + ln)
+            continue
+        else:
+            data = ln.split(': ')
+            if debug:
+                print('Not skipping. Data: ' + str(data))
+            configdict[data[0]] = data[1]
+    if debug:
+        print('\n')
+        for key in configdict.keys():
+            print(key + ': ' + configdict[key])
+        print('\n')
+    return configdict
+
+
+
+class Game():
+    def __init__(self, config='config.txt'):
+        self.config = getConfig(config, debug=False)
+
 
 class Map():
     def __init__(self):
-        try:
-            config = open('config.txt', 'r')
-        except:
-            config = open('defaultconfig.txt', 'r')
-        configdict = {}
-        for ln in range(len(config)):
-            if not ln or ln[0] == '#':
-                continue
-            else:
-                data = ln.split(': ')
-                configdict[data[0]] = data[1]
-        print(configdict)
+        pass
 
-gm_map = Map()
+
+
+
+#gm_map = Map(True)
